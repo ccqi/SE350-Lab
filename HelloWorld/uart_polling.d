d237757a3fc286445f7e333836aca84262a7f124
unknown <y665li@ece-rtos3.NEXUS.UWATERLOO.CA>
2016-01-20 09:38:41 -0500

    helloworld

---
+++ HelloWorld/uart_polling.d
@@ -0,0 +1,8 @@
+uart_polling.o: uart_polling.c
+uart_polling.o: C:\Software\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+uart_polling.o: C:\Software\Keil\ARM\CMSIS\Include\core_cm3.h
+uart_polling.o: C:\Software\Keil\ARM\ARMCC\bin\..\include\stdint.h
+uart_polling.o: C:\Software\Keil\ARM\CMSIS\Include\core_cmInstr.h
+uart_polling.o: C:\Software\Keil\ARM\CMSIS\Include\core_cmFunc.h
+uart_polling.o: C:\Software\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
+uart_polling.o: uart_polling.h

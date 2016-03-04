f32ad53b8b56f39f9f142e7ee006613082fd02f9
eho <ernestho.ho@gmail.com>
2016-03-04 10:35:35 -0500

    Finish test procs for P2

--- RTX/src/usr_proc.c
+++ RTX/src/usr_proc.c
@@ -6,8 +6,6 @@
 #include "printf.h"
 #endif /* DEBUG_0 */
 
-void proc0(void);
-
 /* initialization table item */
 PROC_INIT g_test_procs[NUM_TEST_PROCS];
 
@@ -16,14 +14,21 @@ int test_ok = 0;
 int test_fail = 0;
 int test_num = 6;
 
-int test_2_ok = 0;
+int test_1 = 0;
+int test_2 = 0;
+int test_3 = 0;
+int test_4 = 0;
+int test_5 = 0;
+int test_6 = 0;
 
-int test_3_ok_1 = 0;
-int test_3_ok_2 = 0;
-int test_3_ok_3 = 0;
-int test_3_ok_4 = 0;
+int test_1_ok = 0;
 
-int test_6_ok = 0;
+int test_2_ok_1 = 0;
+int test_2_ok_2 = 0;
+int test_2_ok_3 = 0;
+
+int test_5_ok_1 = 0;
+int test_5_ok_2 = 0;
 
 void print_test_start() {
 	uart1_put_string("G001_test: START\r\n");
@@ -47,6 +52,37 @@ void print_test_FAIL(int n) {
 }
 
 void print_test_results() {
+	if (test_1) {
+		print_test_OK(1);
+	} else {
+		print_test_FAIL(1);
+	}
+	if (test_2) {
+		print_test_OK(2);
+	} else {
+		print_test_FAIL(2);
+	}
+	if (test_3) {
+		print_test_OK(3);
+	} else {
+		print_test_FAIL(3);
+	}
+	if (test_4) {
+		print_test_OK(4);
+	} else {
+		print_test_FAIL(4);
+	}
+	if (test_5) {
+		print_test_OK(5);
+	} else {
+		print_test_FAIL(5);
+	}
+	if (test_6) {
+		print_test_OK(6);
+	} else {
+		print_test_FAIL(6);
+	}
+
 	uart1_put_string("G001_test: ");
 	uart1_put_char((unsigned char)(test_ok + 48));
 	uart1_put_string("/");
@@ -69,284 +105,198 @@ void set_test_procs() {
 		g_test_procs[i].stack_size=0x100;
 	}
   
-	// g_test_procs[0].start_pc = &proc1;
-	g_test_procs[0].start_pc = &proc0;
+	g_test_procs[0].start_pc = &proc1;
 	g_test_procs[1].start_pc = &proc2;
 	g_test_procs[2].start_pc = &proc3;
 	g_test_procs[3].start_pc = &proc4;
 	g_test_procs[4].start_pc = &proc5;
 	g_test_procs[5].start_pc = &proc6;
 
-	g_test_procs[0].priority = HIGH;
+	g_test_procs[0].priority = MEDIUM;
 }
 
-void proc0(void) {
+// TEST DELAYED SEND
+void proc1(void) {
 	int sender_id;
-	int i;
-	char c;
 	MSG *msg;
 
-	while (1) {
-		msg = (MSG*) request_memory_block();
-		msg->type = DEFAULT;
-		msg->text[0] = 'a';
-		msg->text[1] = 'b';
-		msg->text[2] = 'c';
-		msg->text[3] = '\0';
-		delayed_send(1, msg, SECOND);
-		msg = (MSG*) receive_message(&sender_id);
-		i = 0;
-		while (msg->text[i] != '\0') {
-			c = msg->text[i];
-			// uart1_put_char(c);
-			i++;
-		}
-		// uart1_put_char('\n');
-		release_memory_block(msg);
-		release_processor();
-	}
-}
-
-// Request 4 memory blocks, then release all 4.
-// Lowers proc1 to LOWEST priority to run proc2.
-void proc1(void)
-{
-	int i;
-	int ret;
-	void *block1 = NULL;
-	void *block2 = NULL;
-	void *block3 = NULL;
-	void *block4 = NULL;
 	print_test_start();
 
-	// Reduce memory blocks to 4
-	for (i = 0; i < NUM_MEMORY_BLOCKS - 4; i++) {
-		block1 = request_memory_block();
-	}
-
+	msg = (MSG*) request_memory_block();
+	msg->type = DEFAULT;
+	msg->text[0] = 'a';
+	msg->text[1] = 'b';
+	msg->text[2] = 'c';
+	msg->text[3] = '\0';
+	delayed_send(1, msg, 5);
+	set_process_priority(2, LOW);
+	set_process_priority(1, HIGH);
+	msg = (MSG*) receive_message(&sender_id); // goto proc2 until delay finished
+	if (test_1_ok == 1
+			&& sender_id == 1
+			&& msg->type == DEFAULT 
+			&& msg->text[0] == 'a'
+			&& msg->text[1] == 'b'
+			&& msg->text[2] == 'c')
+		test_1 = 1;
+	else
+		test_1 = 0;
+	release_memory_block(msg);
+	set_process_priority(1, LOWEST);
+	
 	while (1) {
-		block1 = request_memory_block();
-		block2 = request_memory_block();
-		block3 = request_memory_block();
-		block4 = request_memory_block();
-		for (i = 0; i < 10000; i++); // Delay
-		ret = release_memory_block(block1);
-		ret = ret || release_memory_block(block2);
-		ret = ret || release_memory_block(block3);
-		ret = ret || release_memory_block(block4);
-
-		if (ret == 0) {
-			print_test_OK(1);
-		} else {
-			print_test_FAIL(1);
-		}
-
-		set_process_priority(1, LOWEST); // Go to proc2
+		release_processor();
 	}
 }
 
-// Request 4 blocks.
-// set to medium priority.
-// set proc3 priority to high.
-// proc2 will be preempted, and proc3 will run.
+// Test message sending, message sending preemption
 void proc2(void)
 {
-	void *block1 = NULL;
-	void *block2 = NULL;
-	void *block3 = NULL;
-	void *block4 = NULL;
+	int i;
+	MSG *msg = (MSG*) request_memory_block();
+	test_1_ok = 1;
+	for (i = 0; i < 1000000; i++) {
+		
+	}
+	set_process_priority(2, LOW);
+	set_process_priority(3, MEDIUM); // go to proc3
 
-	// todo fix stuff
+	if (test_2_ok_1) {
+		test_2_ok_2 = 1;
+	}
+
+	msg->type = DEFAULT;
+	msg->text[0] = '1';
+	msg->text[1] = '\0';
+
+	send_message(3, msg);
+
+	if (test_2_ok_1 && test_2_ok_2 && test_2_ok_3) {
+		test_2 = 1;
+	} else {
+		test_2 = 0;
+	}
+
 	while (1) {
 		release_processor();
 	}
-
-	while (1) {
-		block1 = request_memory_block();
-		block2 = request_memory_block();
-		block3 = request_memory_block();
-		block4 = request_memory_block();
-		set_process_priority(2, MEDIUM);
-		set_process_priority(3, HIGH);
-
-		// proc3 not run after priority set
-		if (!test_2_ok) {
-			print_test_FAIL(2);
-			test_2_ok = -1;
-		}
-
-		release_memory_block(block1);
-		if (!test_3_ok_1) {
-			test_3_ok_1 = -1;
-		}
-		release_memory_block(block2);
-		if (!test_3_ok_2) {
-			test_3_ok_2 = -1;
-		}
-		release_memory_block(block3);
-		if (!test_3_ok_3) {
-			test_3_ok_3 = -1;
-		}
-		release_memory_block(block4);
-		if (!test_3_ok_4) {
-			test_3_ok_4 = -1;
-		}
-	}
 }
 
-// Tests memory blocking with preemption.
-// First request should block process and run proc2
-// Should alternate running proc2 and proc3 until all blocks freed from proc2 and given to proc 3
+// Wait for message from proc2, should come back to this method after message sent
 void proc3(void) {
-	void *block1 = NULL;
-	void *block2 = NULL;
-	void *block3 = NULL;
-	void *block4 = NULL;
+	int sender_id;
+	MSG *msg;
+	test_2_ok_1 = 1;
 
+	msg = (MSG*) receive_message(&sender_id);
+	if (test_2_ok_2 && sender_id == 2 && msg->text[0] == '1' && msg->text[2] == '\0') {
+		test_2_ok_3 = 1;
+	}
+	release_memory_block(msg);
+	set_process_priority(2, MEDIUM);
+	release_processor(); // go to process 2
+
+	set_process_priority(4, MEDIUM);
+	set_process_priority(3, LOWEST);
 	while (1) {
 		release_processor();
 	}
-
-	// proc2 successfully preempted
-	if (test_2_ok != -1) {
-		print_test_OK(2);
-		test_2_ok = 1;
-	}
-
-	while (1) {
-		block1 = request_memory_block();
-		if (!test_3_ok_1) {
-			test_3_ok_1 = 1;
-		}
-		block2 = request_memory_block();
-		if (!test_3_ok_2) {
-			test_3_ok_2 = 1;
-		}
-		block3 = request_memory_block();
-		if (!test_3_ok_3) {
-			test_3_ok_3 = 1;
-		}
-		block4 = request_memory_block();
-		if (!test_3_ok_4) {
-			test_3_ok_4 = 1;
-		}
-
-		if (test_3_ok_1 == 1 && test_3_ok_2 == 1 && test_3_ok_3 == 1 && test_3_ok_4 == 1) {
-			print_test_OK(3);
-		} else {
-			print_test_FAIL(3);
-		}
-
-		release_memory_block(block1);
-		release_memory_block(block2);
-		release_memory_block(block3);
-		release_memory_block(block4);
-
-		// Go to proc4
-		set_process_priority(2, LOWEST);
-		set_process_priority(3, LOWEST);
-	}
 }
 
-// Test for priorities, should stay in proc4 for the test
+// Test request/release memory
 void proc4(void) {
-	int p1;
-	int p2;
-	int p3;
-	int p4;
-	int p5;
-
-	while (1) {
-		release_processor();
-	}
-
-	while (1) {
-		p1 = get_process_priority(1);
-		p2 = get_process_priority(2);
-		p3 = get_process_priority(3);
-		set_process_priority(4, MEDIUM);
-		p4 = get_process_priority(4);
-		set_process_priority(5, LOWEST);
-		p5 = get_process_priority(5);
-		if (p1 == p2 && p2 == p3 && p3 == LOWEST && p4 == MEDIUM && p5 == LOWEST) {
-			print_test_OK(4);
-		} else {
-			print_test_FAIL(4);
-		}
-		// Go to proc5
-		set_process_priority(5, HIGH);
-	}
-}
-
-// Test ok and error return values
-void proc5(void)
-{
-	int ret1;
-	int ret2;
-	int ret3;
-	int ret4;
-	void *block = NULL;
-
-	while (1) {
-		release_processor();
-	}
-
-	while (1) {
-		// Change null priority
-		ret1 = set_process_priority(0, LOWEST);
-		// Change invalid process priority
-		ret2 = set_process_priority(-1, HIGH);
-		// Change process to invalid priority
-		ret3 = set_process_priority(1, -2);
-		// Change process to null process priority
-		ret4 = set_process_priority(2, 4);
-		if (ret1 && ret2 && ret3 && ret4) {
-			ret1 = set_process_priority(1, LOWEST);
-			ret2 = release_memory_block(0);
-			if (!ret1 && ret2) {
-				print_test_OK(5);
-			} else {
-				print_test_FAIL(5);
-			}
-		} else {
-			print_test_FAIL(5);
-		}
-
-		// Go to proc6
-		set_process_priority(6, HIGH);
-		// Since same priority as proc5, proc5 should continue to run
-
+	int i;
+	int ret;
+	void *block;
+	test_3 = 1;
+	for (i = 0; i < 10; i++) {
 		block = request_memory_block();
-		set_process_priority(5, MEDIUM);
-		test_6_ok = 1;
-		release_memory_block(block);
-		// This should never run
-		test_6_ok = 0;
+		if (block == NULL) {
+			test_3 = 0;
+		}
+		ret = release_memory_block(block);
+		if (ret == RTX_ERR) {
+			test_3 = 0;
+		}
+	}
+
+	set_process_priority(5, MEDIUM);
+	set_process_priority(4, LOWEST);
+
+	while (1) {
+		release_processor();
 	}
 }
 
-// Test memory blocking.
-void proc6(void)
-{
+// Test get/set_process_priority
+// Test preemption with same priorities
+void proc5(void)
+{	
 	int i;
 
+	test_4 = 1;
+	set_process_priority(5, HIGH);
+	if (get_process_priority(5) != HIGH) {
+		test_4 = 0;
+	}
+	set_process_priority(1, MEDIUM);
+	set_process_priority(2, LOW);
+	set_process_priority(3, LOWEST);
+	set_process_priority(4, LOW);
+	if (get_process_priority(1) != MEDIUM) {
+		test_4 = 0;
+	}
+	if (get_process_priority(2) != LOW) {
+		test_4 = 0;
+	}
+	if (get_process_priority(3) != LOWEST) {
+		test_4 = 0;
+	}
+	if (get_process_priority(2) != LOW) {
+		test_4 = 0;
+	}
+
+	for (i = 1; i < 5; i++) {
+		set_process_priority(i, LOWEST);
+	}
+
+	set_process_priority(5, HIGH);
+	set_process_priority(6, HIGH);
+	if (test_5_ok_1) {
+		test_5_ok_2 = 1;
+	}
+	release_processor();
+
 	while (1) {
 		release_processor();
 	}
+}
+
+// Test return values.
+void proc6(void)
+{
+	int r1;
+	int r2;
+	int r3;
+	int r4;
+
+	test_5_ok_1 = 1;
+	release_processor();
+	if (test_5_ok_2) {
+		test_5 = 1;
+	}
+
+	r1 = set_process_priority(1, HIGH - 1);
+	r2 = release_memory_block(NULL);
+	r3 = get_process_priority(-1);
+	r4 = set_process_priority(5, LOWEST);
+
+	if (r1 == -1 && r2 == -1 && r3 == -1 && r4 == 0) {
+		test_6 = 1;
+	}
+	print_test_results();
 
 	while (1) {
-		for (i = 0; i < 4; i++) {
-			request_memory_block();
-		}
-		if (test_6_ok) {
-			print_test_OK(6);
-		} else {
-			print_test_FAIL(6);
-		}
-		print_test_results();
-
-		while (1) {
-			release_processor();
-		}
+		release_processor();
 	}
 }
